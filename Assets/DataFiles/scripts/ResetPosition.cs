74a2ca78a7c40ddab01a0f40c657b070df1f85b9
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-02-18 14:40:25 +0530

    Augmented reality animated book

---
+++ Assets/DataFiles/scripts/ResetPosition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 originalPos;
+    void Start()
+    {
+        originalPos=transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collide){
+        if(collide.gameObject.CompareTag("SHIPWALL")){
+            transform.localPosition=originalPos;
+        }
+        else if(collide.gameObject.CompareTag("CLOUDWALL")){
+            transform.localPosition=originalPos;
+        }
+        else if(collide.gameObject.CompareTag("AEROPLANEWALL")){
+            transform.localPosition=originalPos;
+        }
+    }
+    
+}
